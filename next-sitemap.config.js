f4477812d2b7100737bfecdd509cce84818f66af
usman2789 <mangoapple027@gmail.com>
2025-05-11 15:30:39 +0500

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://taleemkasafar.com', // replace with your actual domain
+    generateRobotsTxt: true,           // generates robots.txt file
+    changefreq: 'daily',
+    priority: 0.9,
+    sitemapSize: 5000,
+  }
+  
